7b6ca70c6b26f8c18ec3047e675b4528735bc302
Georgios Deverakis <68283119+GeorgiosDev@users.noreply.github.com>
2022-06-15 12:58:51 +0300

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+
+
+//Dom manipulation
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const messageOne = document.getElementById("message-1")
+const messageTwo = document.getElementById("message-2")
+
+
+messageOne.textContent = "Loading..."
+messageTwo.textContent = ""
+
+
+weatherForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const location = search.value
+    
+    fetch(`/weather?address=${location}`).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageOne.textContent = data.error
+        }
+        else{
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+    
+})
