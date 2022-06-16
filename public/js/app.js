f70fae070c40cc7f47604cf91e2fbd6711947cc9
Georgios Deverakis <68283119+GeorgiosDev@users.noreply.github.com>
2022-06-16 13:00:12 +0300

    Added some styling

--- public/js/app.js
+++ public/js/app.js
@@ -28,3 +28,22 @@ weatherForm.addEventListener("submit", (e)=>{
 })
     
 })
+
+weatherForm.addEventListener("keyup", (e)=> {
+    if(e.keyCode === 13) {
+        e.preventDefault()
+    const location = search.value
+    
+    fetch(`/weather?address=${location}`).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageOne.textContent = data.error
+        }
+        else{
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+    }
+});
